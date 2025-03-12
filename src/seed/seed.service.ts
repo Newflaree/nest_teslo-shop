f780d0a55cf15a4d260aa3d565fbaec1cae028f0
Camilo López <camiloj.lopez79@gmail.com>
2025-03-12 10:55:10 -0300

    Insertar de forma masiva

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -9,6 +10,15 @@ export class SeedService {
 
   async runSeed() {
     await this.insertNewProducts();
+
+    const seedProducts = initialData.products;
+    const insertPromises: any = [];
+
+    seedProducts.forEach( product => {
+      insertPromises.push( this.productsService.create( product ) )
+    });
+
+    await Promise.all( insertPromises );
     return 'SEED EXECUTED'
   }
 
