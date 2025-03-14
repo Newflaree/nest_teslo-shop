af4c5dffc4fad918e8b3c6abfa4964483c761982
Camilo López <camiloj.lopez79@gmail.com>
2025-03-14 10:05:17 -0300

    Servir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,11 @@
+import { Response } from 'express';
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -16,6 +20,16 @@ export class FilesController {
     private readonly filesService: FilesService
   ) {}
 
+  @Get('product/:imageName')
+  findOProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImage( imageName );
+
+    res.sendFile( path );
+  }
+
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
     fileFilter: fileFilter,
@@ -29,10 +43,11 @@ export class FilesController {
     if ( !file )
       throw new BadRequestException( `Make sure that file is an image` );
 
-    console.log( file );
+    //const secureUrl = `${ file.filename }`;
+    const secureUrl = `http://localhost:3000/api/files/product/b8e6b0d1-7bca-4b98-b33e-4e7e267830d7.jpeg`;
 
     return {
-      fileName: file.originalname
+      secureUrl
     };
   }
 }
