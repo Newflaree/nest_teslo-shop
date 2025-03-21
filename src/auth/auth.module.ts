034a3a77df18d652889bd07d795bde595541c90c
Camilo López <camiloj.lopez79@gmail.com>
2025-03-21 11:13:44 -0300

    JwtStrategy - Parte 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,12 +10,17 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy
+  ],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([ User ]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
@@ -39,6 +44,11 @@ import { User } from './entities/user.entity';
     })
      * */
   ],
-  exports: [TypeOrmModule]
+  exports: [
+    TypeOrmModule,
+    JwtStrategy,
+    PassportModule,
+    JwtModule
+  ]
 })
 export class AuthModule {}
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,42 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy( Strategy ) {
-  async valdiate( payload: JwtPayload ): Promise<User> {
-    const { email } = payload;
+  constructor(
+    @InjectRepository(User) 
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
 
-    return ;
+    if ( !jwtSecret ) {
+      throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+
+    super({
+      secretOrKey: jwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate( payload: JwtPayload ): Promise<User> {
+    const { email } = payload;
+    const user = await this.userRepository.findOneBy({ email });
+
+    if ( !user ) 
+      throw new UnauthorizedException('Token not valid');
+
+    if ( !user.isActive )
+      throw new UnauthorizedException('Token not valid!');
+
+    return user;
   }
 }
