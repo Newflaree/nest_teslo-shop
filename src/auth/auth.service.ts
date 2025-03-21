44a70e0675052e0c8c916981d68b6a2513cee93c
Camilo López <camiloj.lopez79@gmail.com>
2025-03-21 18:38:01 -0300

    Tarea: Cambiar email por id en el Payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -40,7 +40,7 @@ export class AuthService {
 
       return {
         ...userDetails,
-        token: this.getJwt({ email: user.email })
+        token: this.getJwt({ id: user.id })
       };
     } catch ( error ) {
       this.handleDBErrors( error );
@@ -52,7 +52,11 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true }
+      select: {
+        email: true,
+        password: true,
+        id: true 
+      }
     });
 
     if ( !user )
@@ -63,7 +67,7 @@ export class AuthService {
 
     return {
       ...user,
-      token: this.getJwt({ email: user.email })
+      token: this.getJwt({ id: user.id })
     };
   }
 
