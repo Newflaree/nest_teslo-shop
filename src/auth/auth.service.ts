97bf13278bc413657d2ed9a998b9289922043fb9
Camilo López <camiloj.lopez79@gmail.com>
2025-03-21 09:37:54 -0300

    Login de usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
 import {
   BadRequestException,
   Injectable,
-  InternalServerErrorException
+  InternalServerErrorException,
+  UnauthorizedException
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import {
+  CreateUserDto,
+  LoginUserDto
+} from './dto';
 
 
 @Injectable()
@@ -37,6 +42,24 @@ export class AuthService {
     }
   }
 
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if ( !user )
+      throw new UnauthorizedException( 'Not valid credentials' )
+
+    if ( !bcrypt.compareSync( password, user.password ) )
+      throw new UnauthorizedException( 'Not valid credentials' )
+
+    // TODO: Return jwt
+    return user;
+  }
+
   private handleDBErrors( error: any ): never {
     if ( error.code === '23505' )
       throw new BadRequestException( error.detail );
@@ -44,5 +67,4 @@ export class AuthService {
     console.log( error );
     throw new InternalServerErrorException( 'Please check server logs' )
   }
-
 }
