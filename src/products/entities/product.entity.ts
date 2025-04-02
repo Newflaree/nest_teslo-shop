896ed8a791b154ee1a26969f01f5ab2c2d19c552
Camilo López <camiloj.lopez79@gmail.com>
2025-04-02 16:05:37 -0300

    Expandir el ApiProperty

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -14,48 +14,78 @@ import { User } from '../../auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
-  @ApiProperty()
+  @ApiProperty({
+    example: '022758bf-53f7-42ab-9e7a-0b1163b8e683',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'T-Shirt Teslo',
+    description: 'Product Title',
+    uniqueItems: true
+  })
   @Column('text', {
     unique: true
   })
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 0,
+    description: 'Product Price'
+  })
   @Column('float', {
     default: 0
   })
   price: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    
+    example: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean risus elit, ullamcorper eget nisl nec, tempus tempor lorem. Vestibulum euismod orci eu tellus convallis, a eleifend metus posuere.',
+    description: 'Product Description',
+    default: null
+  })
   @Column({
     type: 'text',
     nullable: true
   })
   description: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product SLUG - for SEO',
+    uniqueItems: true
+  })
   @Column('text', {
     unique: true
   })
   slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0
+  })
   @Column('int', {
     default: 0
   })
   stock: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['L', 'XL', 'XXL'],
+    description: 'Product Sizes'
+  })
   @Column('text', {
     array: true
   })
   sizes: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'women',
+    description: 'Product Gender'
+  })
   @Column('text')
   gender: string;
 
