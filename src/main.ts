e24c6e306b371334130257b71b4bdb7ca6514639
Camilo López <camiloj.lopez79@gmail.com>
2025-04-02 15:20:02 -0300

    Nestjs Swagger - OpenAPI Specification

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import {
+  Logger,
+  ValidationPipe
+} from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +21,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+
+   const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('Teslo Shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log( `App running on port ${ process.env.PORT }` );
